31bd917f7059766c74691a12341600a01dc3cce5
Berthedusabeyezu <berthe1030@gmail.com>
2019-01-14 11:16:48 +0200

    created new html file

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# project or program name
+QUIZ BROAD
+## author name
+Berthe DUSABEYEZU
+## description of project
+This project asks the user to take a test on JavaScript by answering questions and then display the score achieved by the user.
+
+## project setup instructions
+Must use chrome and edit  textor
+## link to live site on GitHub Pages
+https://github.com/Berthedusabeyezu/QUIZ-BOARD
+
+## copyright and license information
+
+This project is under MIT License. Copyright (c) 2018 Berthe
+
+
---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+var score = 0;
+var complete = 0;
+var calculate = function(){
+  for (i = 1; i <= 5; i++) {
+    var response = $("input:radio[name=js"+i+"]:checked").val();
+    score += parseInt(response);
+    if (response != undefined) {
+      complete += 1
+    }
+  } 
+}
+
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    calculate();
+    var message = ""
+    var remark =""
+    if (complete === 5) {
+      message = "Your score is:";
+      $("#display").text(score+"%").fadeIn();
+      if (score <= 20) {
+        $("#remark").append("<' alt='Bad'>");
+      } else if (score <= 60) {
+        $("#remark").append("<' alt='Average'>");
+      } else if (score <= 80) {
+        $("#remark").append("< alt='Good'>");
+      } else {
+        $("#remark").append("< alt='Excellent!'>");
+      }
+    } else {
+      message = "Please answer all the questions and submit again!";
+    }
+    $("#message").text(message);
+    $(".carousel").remove();
+    $("button.bg-success").fadeOut("slow");
+    $("div.your-score").slideUp(50);
+    $("div.your-score").slideDown(1500);
+  })
+}) 
+function reload() {
+  location.reload();
+} 
